4c0f1a46380d1e40cf2129c4d3ac5d685b360aab
Oleg Hansen <oleg-ucb@outlook.com>
2020-05-08 12:45:42 -0700

    solution

--- prodExCurrent.js
+++ prodExCurrent.js
@@ -1,16 +1,32 @@
 /*
-Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
-
-For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
-
-Follow-up: what if you can't use division?
+Given arr of ints, return a new arr such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+IN: [1, 2, 3, 4, 5], OUT: [120, 60, 40, 30, 24]. AND IN: [3, 2, 1], OUT: [2, 3, 6].  Follow-up: what if you can't use division?
 */
+function arrayRemove(arr, value) {
+     return arr.filter(function(ele){ return ele != value; });} 
+function multArr(arr){
+    if (arr.length == 0) return 0;
+    var result = 1;
+    for (let i=0; i<arr.length;i++){
+        if(arr[i] == 0){return 0;}
+        result = arr[i] * result;
+    }
+    return result
+}
 function retProds(arr){
     let solved=[];
 
-    
-
+    for(let i = 0; i<arr.length; i++){
+        var removed = arr[i];
+        var l = arr[i];
+        var left = arrayRemove(arr, l);
+        var res=0;
+        res = multArr(left);
+        solved.push(res)
+    }
+    console.log('SOLVED IS    ***********     ',solved);
     return solved;
 }
 
 retProds([3, 2, 1]);
+retProds([1, 2, 3, 4, 5]);
