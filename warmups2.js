1d84a1fd8a7b152d4c931eb0e265ddcd578a8927
Oleg H <oleg-ucb@outlook.com>
2020-05-19 15:19:13 -0700

    warmups

--- warmups2.js
+++ warmups2.js
@@ -1,28 +1,30 @@
-function spinalCase(str) {
-
-    var str1 = str.replace(/([a-z])([A-Z])/g, "$1 $2"); /// stuck on this solution- replaces lower-followed-by-cap with space
-    str1 = str1.replace(/\s+|[_]/g, "-"); // replaces spaces and or udnerscores with DASH
-    // Replace low-upper case to low-space-uppercase
-    //str1 = str1.replace(/([a-z])([A-Z])/g, "$1 $2");
-    str1 = str1.toLowerCase();
-    console.log(str1);
-    return str1;
+class Book {
+    constructor(title, pages, isbn) {
+        this.title = title;
+        this.plages = pages,
+            this.isbn = isbn;
+    }
+    printIsbn() { console.log(this.isbn); }
 }
 
-spinalCase('This Is Spinal Tap');
+function Book2(title, pages, isbn) {
+    this.title = title;
+    this.pages = pages;
+    this.isbn = isbn;
+}
 
-//1. This Is Spinal Tap
-//>>>> MAKE THIS LAST STEP - convert to lower
-// if space or underscore convert to dash
-// if underscore convert to dash
-// if lower followed by upper - convert insert dash
+Book2.prototype.printTitle = function() {
+    console.log(this.title);
+};
 
-//spinalCase('This Is Spinal Tap');
+class ItBook extends Book2 {
+    constructor(title, pages, isbn, technology) {
+        super(title, pages, isbn);
+        this.technology = technology;
+    }
+    printTechnology() { console.log(this.technology); }
+}
 
-/*
-spinalCase("This Is Spinal Tap") should return "this-is-spinal-tap".
-spinalCase("thisIsSpinalTap") should return "this-is-spinal-tap".
-spinalCase("The_Andy_Griffith_Show") should return "the-andy-griffith-show".
-spinalCase("Teletubbies say Eh-oh") should return "teletubbies-say-eh-oh".
-spinalCase("AllThe-small Things") should return "all-the-small-things" 
-*/
+let jsBook = new ItBook('JS algos', 2000, '123', 'JS')
+console.log(jsBook.title);
+console.log(jsBook.printTechnology());
