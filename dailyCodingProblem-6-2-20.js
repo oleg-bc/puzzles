1e6414b3c2033db0f851d43b7e9b8237b578fbeb
Oleg H <oleg-ucb@outlook.com>
2020-06-02 11:53:58 -0700

    tuesday june 2 daily coding problem - working on problem

--- dailyCodingProblem-6-2-20.js
+++ dailyCodingProblem-6-2-20.js
@@ -1,5 +1,5 @@
 /*
-This problem was asked by Facebook.
+Was asked by Facebook.
 Given a string of round, curly, and square open and closing brackets,
 return whether the brackets are balanced(well - formed).
 IN: "([])[]({})" //true.
@@ -8,29 +8,46 @@ IN: "((()" // false.
 */
 
 var isEven = function(strIn) {
+
     let poCounter = 0;
     let soCounter = 0;
+    let coCounter = 0;
     let pcCounter = 0;
     let scCounter = 0;
-    let openP = false;
-    let openS = false;
-    for (var i = 0; i < strIn.length; i++) {
-        if (i === '(') {
+    let ccCounter = 0;
+
+    let pOpen = false;
+    let sOpen = false;
+    let cOpen = false;
+
+    for (let i = 0; i < strIn.length; i++) {
+        if (strIn[i] === '(') {
+            pOpen = true;
             poCounter++;
-            openP = true;
         }
-        if (i === '[') {
+        if (strIn[i] === '[') {
+            sOpen = true;
             soCounter++;
-            openS = true;
+        }
+        if (strIn[i] === '{') {
+            cOpen = true;
+            coCounter++;
         }
 
-
-        //closing
-        if (i === ')') {
+        if (strIn[i] === ')') {
+            pOpen = false;
             pcCounter++;
-            if (openS == true) { return false; }
         }
-        if (i === ']') { scCounter++; }
+        if (strIn[i] === ']') {
+            sOpen = false;
+            scCounter++;
+        }
+        if (strIn[i] === '}') {
+            cOpen = false;
+            scCounter++;
+        }
+
+        if (strIn[i] === ')' && (sOpen || cOpen)) { return false; }
     }
     if (poCounter === pcCounter && soCounter === scCounter) { return true; }
     return false;
@@ -39,3 +56,35 @@ var isEven = function(strIn) {
 console.log(isEven("([])[]({})")); //true.
 console.log(isEven("([)]")); // false.
 console.log(isEven("((()")); // false.
+
+
+
+/*
+    let poCounter = 0;
+    let soCounter = 0;
+    let pcCounter = 0;
+    let scCounter = 0;
+    let openP = false;
+    let openS = false;
+    for (var i = 0; i < strIn.length; i++) {
+        if (strIn[i] === '(') {
+            poCounter++;
+            openP = true;
+        }
+        if (strIn[i] === '[') {
+            soCounter++;
+            openS = true;
+        }
+        //closing
+        if (i === ']') {
+            scCounter++;
+            openS = false;
+        }
+        if (strIn[i] === ')') {
+            pcCounter++;
+            if (openS == true) { return false; }
+        }
+    }
+    if (poCounter === pcCounter && soCounter === scCounter) { return true; }
+    return false;
+*/
