eda7a162f4c99f086b32418c478c90e7c0a0e323
Oleg H <oleg-ucb@outlook.com>
2020-06-08 11:12:43 -0700

    runningMedian problem

---
+++ runningMedian-Easy.js
@@ -0,0 +1,22 @@
+/*Was asked by Microsoft.
+Compute the running median of a sequence of numbers. 
+Given a stream of numbers, print out the median of the list so far on each new element.
+The median of an even-numbered list is the average of the two middle numbers.
+For example, given the sequence [2, 1, 5, 7, 2, 0, 5], your algorithm should print out:
+2
+1.5
+2
+3.5
+2
+2
+2
+*/
+
+function runningMedian(args) {
+    console.log(args[0]);
+    for (let i = 1; i < args.length; i++) {
+        if (args[])
+    }
+}
+
+runningMedian([2, 1, 5, 7, 2, 0, 5]);
