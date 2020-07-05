59b76d06540a9f45b5a6f53a10f83e4ef70a2903
Oleg H <oleg-ucb@outlook.com>
2020-07-04 17:06:40 -0700

    fixed bug

--- isSubtree.js
+++ isSubtree.js
@@ -11,7 +11,7 @@ function isIdent(t1, t2) {
     if (t1 !== null && t2 !== null) {
         if (t1.value === t2.value &&
             isIdent(t1.left, t2.left) &&
-            isIdent(t2.right, t2.right)) return true;
+            isIdent(t1.right, t2.right)) return true;
     }
     return false;
 }
@@ -24,4 +24,64 @@ function isSubtree(t1, t2) {
 
     return (isSubtree(t1.left, t2) || isSubtree(t1.right, t2));
 
+}
+
+
+let v1 = {
+    "value": 5,
+    "left": {
+        "value": 10,
+        "left": {
+            "value": 4,
+            "left": {
+                "value": 1,
+                "left": null,
+                "right": null
+            },
+            "right": {
+                "value": 2,
+                "left": null,
+                "right": null
+            }
+        },
+        "right": {
+            "value": 6,
+            "left": {
+                "value": -1,
+                "left": null,
+                "right": null
+            },
+            "right": null
+        }
+    },
+    "right": {
+        "value": 7,
+        "left": null,
+        "right": null
+    }
+}
+let v2 = {
+    "value": 10,
+    "left": {
+        "value": 4,
+        "left": {
+            "value": 1,
+            "left": null,
+            "right": null
+        },
+        "right": {
+            "value": 2,
+            "left": null,
+            "right": null
+        }
+    },
+    "right": {
+        "value": 6,
+        "left": null,
+        "right": {
+            "value": -1,
+            "left": null,
+            "right": null
+        }
+    }
 }
