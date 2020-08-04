cca0ae524a709d8b08ee632e2acd95f1b483f948
Oleg H <oleg-ucb@outlook.com>
2020-08-04 16:44:00 -0700

    solved more cases

--- dcp90.js
+++ dcp90.js
@@ -7,9 +7,9 @@ Given an integer n and a list of integers l, write a function that
 randomly generates a number from 0 to n-1 that isn't in l (uniform).
 */
 function retRand(num, list) {
-
+    let themult = Math.floor(num);
     let retVal;
-    retVal = Math.floor(10 * (Math.random()));
+    retVal = Math.floor(themult * (Math.random()));
 
 
     if (list.includes(retVal)) {
@@ -22,6 +22,6 @@ function retRand(num, list) {
 }
 
 
-let l = [1, 2, 3, 4, 5]
-let n = 8;
+let l = [1, 2, 3, 4, 5, 88, 676]
+let n = 888;
 console.log(retRand(n, l));
