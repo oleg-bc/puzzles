9f56a17d9e0eae93d73af3e527d8d44d95d5d9f6
Oleg H <oleg-ucb@outlook.com>
2020-08-19 11:33:33 -0700

    another puzzle in warmups

--- warmups3.js
+++ warmups3.js
@@ -12,4 +12,27 @@ function maxConsecutiveSum(arr) {
     }
     return ultimate ? ultimate : 0;
 }
-console.log(maxConsecutiveSum([8, -1, 3, -4, 5, -25, 5])); /// going thru this number by number it makes sense how the choice works
+//console.log(maxConsecutiveSum([8, -1, 3, -4, 5, -25, 5])); /// going thru this number by number it makes sense how the choice works
+
+function latticePaths(m, n) {
+
+
+    let cache = {};
+
+    function traverse(m, n) {
+
+        let key = m + "_" + n;
+
+        if (key in cache) { return cache[key]; }
+        if (m < 0 || n < 0) { return 0; }
+        if (m === 0 && n === 0) { return 1; }
+
+        cache[key] = traverse(m - 1, n) + traverse(m, n - 1);
+        return cache[key];
+    }
+    return traverse(m, n);
+
+}
+
+console.log(latticePaths(3, 2));
+console.log(latticePaths(7, 3));
