8e8ba2969d50951888f838f0e4a88e144035e0ae
Oleg H <oleg-ucb@outlook.com>
2020-08-18 17:26:03 -0700

    reviewing another puzzle

--- warmups4.js
+++ warmups4.js
@@ -1,3 +1,31 @@
+//House Rober return max $
+
+function houseRobber(nums) {
+    if (nums.length == 1) { return [0]; }
+    if (nums.length == 0) { return 0; }
+    let sorter = [];
+
+    sorter[0] = nums[0]; /// sorter gets the 1st element from input
+    sorter[1] = Math.max(nums[1], nums[0]); /// the 2nd element in sorter is a bigger of 1st and 2nd elems of input
+
+    for (let i = 2; i < nums.length; i++) {
+        let A = sorter[0]; // A is the 0th element of sorter
+        let B = sorter[1]; // B is 1st
+        current = nums[i]; // current is element at position 3 in input
+        sorter[0] = B; // replace sorter at 0th position with B --- the 1st element AND THE BIGGER element
+        sorter[1] = Math.max(A + current, B); /// REPLACE 1st position element of sorter with the bigger of POSITION ONE OR 2
+    }
+    return sorter[1];
+}
+
+
+let robTest1 = [1, 2, 3, 1];
+let robTest2 = [2, 7, 9, 3, 1]
+
+console.log(houseRobber(robTest1));
+console.log(houseRobber(robTest2));
+
+
 ///DYNAMIC PROG MOVING WINDOW BIT FLIP
 /// a true moving window algo takes a while to grok
 /// we're closing the window only when we get to e
@@ -22,4 +50,5 @@ function bitFlip(arr, n) {
     return max;
 }
 
-console.log(bitFlip([0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1], 2));
+// console.log(bitFlip([0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1], 2));
+// console.log(bitFlip([0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1], 2));
