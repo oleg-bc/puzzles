9cc97cfd8d1e56f6e621ac61929759ce08c0c178
Oleg Hansen <oleg-ucb@outlook.com>
2020-09-23 10:13:25 -0700

    prob 5

--- ic/ic-p5.js
+++ ic/ic-p5.js
@@ -47,8 +47,55 @@ function isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders) {
     return false;}
   }
 
-  
+
   let to = [1, 3, 5], dos=[2, 4, 6], so = [1, 2, 4, 6, 5, 3]
   //let to = [17, 8, 24], dos= [12, 19, 2], so=[17, 8, 12, 19, 24, 2]
 
   console.log(isFirstComeFirstServed(to,dos,so));
+
+
+  ///note theres one test case in the above problem thats still not solved
+//official solution:
+  /*
+  function isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders) {
+    var takeOutOrdersIndex = 0;
+    var dineInOrdersIndex = 0;
+    var takeOutOrdersMaxIndex = takeOutOrders.length - 1;
+    var dineInOrdersMaxIndex = dineInOrders.length - 1;
+
+    for (var i = 0; i < servedOrders.length; i++) {
+        var order = servedOrders[i];
+
+        // if we still have orders in takeOutOrders
+        // and the current order in takeOutOrders is the same
+        // as the current order in servedOrders
+        if (takeOutOrdersIndex <= takeOutOrdersMaxIndex &&
+                order === takeOutOrders[takeOutOrdersIndex]) {
+            takeOutOrdersIndex++;
+
+        // if we still have orders in dineInOrders
+        // and the current order in dineInOrders is the same
+        // as the current order in servedOrders
+        } else if (dineInOrdersIndex <= dineInOrdersMaxIndex &&
+                order === dineInOrders[dineInOrdersIndex]) {
+            dineInOrdersIndex++;
+
+        // if the current order in servedOrders doesn't match the current
+        // order in takeOutOrders or dineInOrders, then we're not serving first-come,
+        // first-served
+        } else {
+            return false;
+        }
+    }
+
+    // check for any extra orders at the end of takeOutOrders or dineInOrders
+    if (dineInOrdersIndex != dineInOrders.length ||
+           takeOutOrdersIndex != takeOutOrders.length) {
+        return false;
+    }
+
+    // all orders in servedOrders have been "accounted for"
+    // so we're serving first-come, first-served!
+    return true;
+}
+  */
