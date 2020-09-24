8d9e5a7e8baf12332352ff7c21cf7d9b352575da
Oleg Hansen <oleg-ucb@outlook.com>
2020-09-24 16:14:52 -0700

    added the solution to prob 9

---
+++ ic/ic-p9.js
@@ -0,0 +1,74 @@
+/*
+Write a function that takes:
+
+an array of unsortedScores
+the highestPossibleScore in the game
+and returns a sorted array of scores in less than O(n\lg{n})O(nlgn) time.
+
+For example:
+
+  const unsortedScores = [37, 89, 41, 65, 91, 53];
+const HIGHEST_POSSIBLE_SCORE = 100;
+
+sortScores(unsortedScores, HIGHEST_POSSIBLE_SCORE);
+// returns [91, 89, 65, 53, 41, 37]
+*/
+
+function sortScores(unorderedScores, highestPossibleScore) {
+
+    // Sort the scores in O(n) time
+  
+    return unorderedScores;
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // Tests
+  
+  let desc = 'no scores';
+  let actual = sortScores([], 100);
+  let expected = [];
+  assertEqual(JSON.stringify(actual), JSON.stringify(expected), desc);
+  
+  desc = 'one score';
+  actual = sortScores([55], 100);
+  expected = [55];
+  assertEqual(JSON.stringify(actual), JSON.stringify(expected), desc);
+  
+  desc = 'two scores';
+  actual = sortScores([30, 60], 100);
+  expected = [60, 30];
+  assertEqual(JSON.stringify(actual), JSON.stringify(expected), desc);
+  
+  desc = 'many scores';
+  actual = sortScores([37, 89, 41, 65, 91, 53], 100);
+  expected = [91, 89, 65, 53, 41, 37];
+  assertEqual(JSON.stringify(actual), JSON.stringify(expected), desc);
+  
+  desc = 'repeated scores';
+  actual = sortScores([20, 10, 30, 30, 10, 20], 100);
+  expected = [30, 30, 20, 20, 10, 10];
+  assertEqual(JSON.stringify(actual), JSON.stringify(expected), desc);
+  
+  function assertEqual(a, b, desc) {
+    if (a === b) {
+      console.log(`${desc} ... PASS`);
+    } else {
+      console.log(`${desc} ... FAIL: ${a} != ${b}`);
+    }
+  }
